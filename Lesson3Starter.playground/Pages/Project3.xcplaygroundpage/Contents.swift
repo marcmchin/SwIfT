80a3a91e98c3c28f861944595a86ed0c8c159505
Marcus Chin <Marcus@Marcuss-MacBook-Air.local>
2017-09-23 18:02:40 +0800

    Upload Swift Playground Lesson 3

---
+++ Lesson3Starter.playground/Pages/Project3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,56 @@
+//: [Previous](@previous)
+
+import Foundation
+
+setBotTitle("My Awesome Dance")
+setBotSubtitle("By Ken")
+
+startBot()
+
+
+
+//routine 1
+func routine1() {
+    leftArmUp()
+    leftLegUp()
+    fabulize()
+    leftArmDown()
+    leftLegDown()
+    shakeItLeft()
+    shakeItRight()
+    jumpUp()
+    shakeItCenter()
+    jumpDown()
+    shakeItRight()
+    shakeItLeft()
+    defabulize()
+}
+//routine 2
+func routine2() {
+    fabulize()
+    defabulize()
+    fabulize()
+    defabulize()
+    fabulize()
+    leftArmUp()
+    rightArmUp()
+    leftLegUp()
+    rightLegUp()
+    leftArmDown()
+    rightArmDown()
+    leftLegDown()
+    rightLegDown()
+    shakeItLeft()
+    shakeItRight()
+    shakeItCenter()
+    shakeItCenter()
+    shakeItRight()
+    shakeItLeft()
+    defabulize()
+}
+
+
+//routined so that all the changes can be done at 1 location instead of all over the place
+
+
+//: [Next](@next)
